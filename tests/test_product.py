8af8b4a188e769b6636a6eaa1b7fa19430d4ea5d
aleksandr <kharitonov_am@bk.ru>
2025-01-10 12:14:42 +0300

    added a test for creating objects of the Product class

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,1 +1,10 @@
 import pytest
+
+
+def test_making_object_Product(my_phone):
+    '''Тестируем, что функционал по созданию объекта класса Product работает корректно:'''
+    my_phone_object = my_phone
+    assert my_phone_object.name == "Iphone 15"
+    assert my_phone_object.description == "512GB, Gray space"
+    assert my_phone_object.price == 210000.0
+    assert my_phone_object.quantity == 8
