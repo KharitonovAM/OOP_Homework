0bfccc7baf6cb91d53d35999d0c12631ce8995e2
aleksandr <kharitonov_am@bk.ru>
2025-01-10 17:00:32 +0300

    Add lod setting to setting

--- setting/setting.py
+++ setting/setting.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+import logging
+
 
 BASE_DIR = Path(__file__).parent.parent
 json_file = Path(BASE_DIR, 'data','products.json')
+log_file = Path(BASE_DIR, 'logs', 'logfile.txt')
+
+my_log_config = logging.basicConfig(level=logging.DEBUG,
+                                    format="%(asctime)s - %(levelname)s- %(name)s - %(message)s",
+                                    filename=log_file,
+                                    filemode='w')
