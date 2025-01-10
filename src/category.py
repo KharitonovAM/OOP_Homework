7a2eef8d5d165504262334effdf95c633fcc22d1
aleksandr <kharitonov_am@bk.ru>
2025-01-10 17:08:50 +0300

    covered the category module with logging

--- src/category.py
+++ src/category.py
@@ -1,3 +1,13 @@
+import logging
+from setting.setting import my_log_config
+
+
+# импортируем настройки логирования
+logging.basicConfig = my_log_config
+
+# определяем именные логеры
+logging_category = logging.getLogger("class_Category")
+
 class Category:
     '''Класс по созданию объектов Category'''
 
@@ -10,9 +20,11 @@ class Category:
     def __init__(self, name, description, products) -> None:
         '''Инициализация объекта класса Category'''
 
+        logging_category.info('Начало инициации объекта класса Category')
         self.name = name
         self.description = description
         self.products = products
         Category.product_count += len(products)
         Category.category_count += 1
+        logging_category.info(f'Завершили инициацию объекта класса Category с параметрами name - {name}, description - {description}, products - {products}')
 
