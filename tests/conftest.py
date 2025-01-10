6103a644fc115a7e04ef2f84c4f22de91306e741
aleksandr <kharitonov_am@bk.ru>
2025-01-10 12:38:57 +0300

    add test_make_object_category for testinng creation objects of Category and fixture phone_category. ImpotrError when testing with pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 from src.product import Product
+from src.category import Category
 
 @pytest.fixture
 def my_phone():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+@pytest.fixture
+def phone_category():
+    return Category("Смартфоны",
+                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+                         ['Iphone 15', 'Iphone 14', 'Iphone 13'])
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,1 +1,9 @@
 import pytest
+
+def test_make_object_category(phone_category):
+    '''Тестируем, что функционал по созданию объекта класса Category работает корректно'''
+
+    test_category = phone_category
+    assert test_category.name == "Смартфоны"
+    assert test_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    assert test_category.description == ['Iphone 15', 'Iphone 14', 'Iphone 13']
