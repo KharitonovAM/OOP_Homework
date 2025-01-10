ee5d8434602a1395e29368beef4a6ee7b9d00f60
aleksandr <kharitonov_am@bk.ru>
2025-01-10 14:56:16 +0300

    implemented a function to get data from a file. The test is successful. Coverage 100%

---
+++ setting/setting.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+json_file = Path('data','products.json')
--- src/init.py
+++ src/init.py
@@ -1,3 +1,9 @@
+import json
+
+
 def take_data_from_json(filename):
     '''Принимает на вход путь к json файлу, возвращает словать с информцей из json-файла '''
-    pass
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        data_from_file = json.load(file)
+    return data_from_file
