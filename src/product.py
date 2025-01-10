830764bb3479f2f5202bf409e47699876577525f
aleksandr <kharitonov_am@bk.ru>
2025-01-10 12:24:53 +0300

    added the initialization code to the Product class. The test_making_object_Product is working correctly

--- src/product.py
+++ src/product.py
@@ -1,2 +1,13 @@
 class Product:
-    pass
+    """Класс по созданию объектов Продукт"""
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        '''Инициализация объекта класса Product'''
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
