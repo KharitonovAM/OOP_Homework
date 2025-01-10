1fc4f61e84603a0b6b95298fefd94896ec0d0750
aleksandr <kharitonov_am@bk.ru>
2025-01-10 17:28:23 +0300

    change endcoding of log-file

--- setting/setting.py
+++ setting/setting.py
@@ -9,4 +9,5 @@ log_file = Path(BASE_DIR, 'logs', 'logfile.txt')
 my_log_config = logging.basicConfig(level=logging.DEBUG,
                                     format="%(asctime)s - %(levelname)s- %(name)s - %(message)s",
                                     filename=log_file,
-                                    filemode='w')
+                                    filemode='w',
+                                    encoding='UTF-8')
