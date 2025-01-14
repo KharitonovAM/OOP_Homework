6c3dd370cd4df0db759785dcf79e5b3a51fe129f
aleksandr <kharitonov_am@bk.ru>
2025-01-14 19:40:38 +0300

    Выполнил тест для тестирования new_product с учетом нового расширенного функционала, при выполнении тестирования TypeError

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,9 +1,10 @@
 import pytest
+from typing import Any
 
 from src.product import Product
 
 
-def test_making_object_Product(my_phone : Product) -> None:
+def test_making_object_Product(my_phone: Product) -> None:
     """Тестируем, что функционал по созданию объекта класса Product работает корректно:"""
 
     my_phone_object = my_phone
@@ -20,17 +21,36 @@ def test_not_enough_variables() -> None:
         Product("somename", "somedescription")
 
 
-def test_new_product1(product_dict1):
+def test_new_product1(product_dict1: dict[str, Any]) -> None:
     '''Проверяет функционал метода new_product, принимающий словарь и возвращающь экземпляр класса Product'''
     assert Product.new_product(product_dict1).name == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14).name
-    assert Product.new_product(product_dict1).description == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14).description
-    assert Product.new_product(product_dict1).price == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14).price
-    assert Product.new_product(product_dict1).quantity == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14).quantity
+    assert Product.new_product(product_dict1).description == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0,
+                                                                     14).description
+    assert Product.new_product(product_dict1).price == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0,
+                                                               14).price
+    assert Product.new_product(product_dict1).quantity == Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0,
+                                                                  14).quantity
 
 
-def test_new_product1(product_dict2):
+def test_new_product2(product_dict2: dict[str, Any]) -> None:
     '''Проверяет функционал метода new_product, принимающий словарь и возвращающь экземпляр класса Product'''
+
     assert Product.new_product(product_dict2).name == Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7).name
-    assert Product.new_product(product_dict2).description == Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7).description
+    assert Product.new_product(product_dict2).description == Product('55" QLED 4K', "Фоновая подсветка", 123000.0,
+                                                                     7).description
     assert Product.new_product(product_dict2).price == Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7).price
-    assert Product.new_product(product_dict2).quantity == Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7).quantity
+    assert Product.new_product(product_dict2).quantity == Product('55" QLED 4K', "Фоновая подсветка", 123000.0,
+                                                                  7).quantity
+
+
+def test_new_product_upgrade(my_phone: Product):
+    '''Тестирует расширенный функционал new_product с проверкой наличия добавляемого продукта уже в списке продуктов'''
+
+    my_list = [my_phone]
+    Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert Product.new_product(
+        {'name': 'Iphone 15', 'description': "512GB, Gray space", 'price': 15000, 'quantity': 12},
+        my_list).price == 210000.0
+    assert Product.new_product(
+        {'name': 'Iphone 15', 'description': "512GB, Gray space", 'price': 15000, 'quantity': 12},
+        my_list).quantity == 20
