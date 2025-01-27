2182c32c311ec40ec717c9595c20376cc7583b9f
aleksandr <kharitonov_am@bk.ru>
2025-01-28 00:38:36 +0300

    Добавил класс по обработке исключений в отдельный модуль raises ZeroQuantityError

---
+++ src/raises.py
@@ -0,0 +1,11 @@
+class ZeroQuantityError(Exception):
+    '''Класс обработки ошибок при добавлении продукта с количествоим 0'''
+
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Добавлен продукт с количеством 0'
+
+    def __str__(self):
+        return self.message
+
+
+
